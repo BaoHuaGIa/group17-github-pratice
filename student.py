6278940d7d118b8da114eee3a227cee1330d1429
Khanhthao <23521469@gm.uit.edu.vn>
2026-05-15 09:32:03 +0700

    Hoàn thành tính năng hiển thị sinh viên

---
+++ student.py
@@ -0,0 +1,21 @@
+class Student:
+    def __init__(self, student_id, name, major):
+        self.student_id = student_id
+        self.name = name
+        self.major = major
+
+    def __str__(self):
+        return f"[{self.student_id}] {self.name} - Chuyên ngành: {self.major}"
+
+class StudentManager:
+    def __init__(self):
+        self.students = []
+
+    def display_students(self):
+        """Hiển thị tất cả sinh viên"""
+        if not self.students:
+            print("-> Danh sách đang trống.")
+            return
+        
+        for index, student in enumerate(self.students, 1):
+            print(f"{index}. {student}")
